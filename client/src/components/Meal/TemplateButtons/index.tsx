8cde78d8a29899826782b0520833b52cb4f2fbc7
qkrrlgh519 <qkrrlgh519@naver.com>
2020-11-26 03:24:41 +0900

    [feat] 서식 버튼 UI 구현

---
+++ client/src/components/Meal/TemplateButtons/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import * as S from './style';
+import { Button, Icon } from 'semantic-ui-react';
+
+function TemplateButtons() {
+  return (
+    <S.TemplateButtonsContainer>
+      <Button.Group basic>
+        <Button icon="text height" />
+        <Button icon="tint" />
+      </Button.Group>{' '}
+      <Button.Group basic>
+        <Button icon="align left" />
+        <Button icon="align center" />
+        <Button icon="align right" />
+      </Button.Group>
+    </S.TemplateButtonsContainer>
+  );
+}
+
+export default TemplateButtons;
