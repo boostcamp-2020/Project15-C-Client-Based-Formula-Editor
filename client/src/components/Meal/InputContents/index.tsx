112772108bf0afb738080e76d0ec1ef4712b594a
WooDong <tmlu48@naver.com>
2020-11-25 22:24:43 +0900

    [feat] 메인 컨텐츠 컴포넌트 레이아웃 구현

---
+++ client/src/components/Meal/InputContents/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import InputContentsContainer from './style';
+
+function InputContents() {
+  return <InputContentsContainer></InputContentsContainer>;
+}
+
+export default InputContentsContainer;
--- client/src/components/Meal/Main/index.tsx
+++ client/src/components/Meal/Main/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import MainContentsContainer from './style';
-import { Tab } from 'semantic-ui-react';
+import InputContents from '../InputContents';
+import OutputContents from '../OutputContents';
 import styled from '@emotion/styled';
 
 function MainContents() {
-  return <MainContentsContainer></MainContentsContainer>;
+  return (
+    <MainContentsContainer>
+      <InputContents></InputContents>
+      <OutputContents></OutputContents>
+    </MainContentsContainer>
+  );
 }
 
 export default MainContents;
