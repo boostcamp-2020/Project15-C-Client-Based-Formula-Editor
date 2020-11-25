1318ee6723d55a85e1676aee0d2c5a0fc2d9f4a3
WooDong <tmlu48@naver.com>
2020-11-26 03:54:18 +0900

    [feature] 수식탭 추가 및 제거 기능 구현

--- client/src/components/Ingredients/ButtonItem/index.tsx
+++ client/src/components/Ingredients/ButtonItem/index.tsx
@@ -4,11 +4,12 @@ interface ButtonItemProps {
   title?: string;
   icon?: any;
   size?: 'big' | 'small' | 'mini' | 'tiny' | 'medium' | 'large' | 'huge' | 'massive' | undefined;
+  handler: () => void;
 }
 
-function ButtonItem({ title, icon, size }: ButtonItemProps) {
+function ButtonItem({ title, icon, size, handler }: ButtonItemProps) {
   return (
-    <Button icon size={size}>
+    <Button icon size={size} onClick={handler}>
       {title}
       {icon ? <Icon name={icon} /> : ''}
     </Button>
--- client/src/components/Meal/Tab/index.tsx
+++ client/src/components/Meal/Tab/index.tsx
@@ -4,28 +4,54 @@ import { TabContainer } from './style';
 import { Icon } from 'semantic-ui-react';
 import Button from '../../Ingredients/ButtonItem';
 
+interface TabType {
+  id: number;
+  name: string;
+}
+
 function Tab() {
-  const [tabState, setTabState] = useState({ activeItem: '1' });
-  const handleItemClick = (name: string) => setTabState({ activeItem: name });
-  const { activeItem } = tabState;
+  const [tabState, setTabState] = useState({ activeItem: 1 });
+  const handleItemClick = (id: number) => setTabState({ activeItem: id });
+  const [tabListState, setTabListState] = useState<(TabType | undefined)[]>([
+    { id: 1, name: '수식' },
+  ]);
+  const addTabHandler = () => {
+    const index = tabListState.length + 1;
+    setTabListState([...tabListState, { id: index, name: '수식' }]);
+  };
+  const removeTabHandloer = (id: number) => {
+    console.log('dd');
+    const tabList: (TabType | undefined)[] = tabListState.filter((item: TabType | undefined) => {
+      if (item && id !== item.id) {
+        if (item.id > id) item.id -= 1;
+        return item;
+      }
+    });
+    setTabListState([...tabList]);
+  };
 
   return (
     <TabContainer>
       <Menu pointing vertical>
-        <Menu.Item name="inbox" active={activeItem === '1'} onClick={() => handleItemClick('1')}>
-          <Label size="mini" color="teal">
-            X
-          </Label>
-          수식 1
-        </Menu.Item>
-        <Menu.Item name="inbox" active={activeItem === '2'} onClick={() => handleItemClick('2')}>
-          <Label size="mini" color="teal">
-            X
-          </Label>
-          수식 2
-        </Menu.Item>
+        {tabListState.map((item: TabType | undefined, index) => {
+          if (item) {
+            return (
+              <Menu.Item
+                key={index}
+                name={item.name}
+                active={tabState.activeItem === item.id}
+                onClick={() => handleItemClick(item.id)}
+              >
+                <Label size="mini" color="teal" onClick={() => removeTabHandloer(item.id)}>
+                  X
+                </Label>
+                수식 {item.id}
+              </Menu.Item>
+            );
+          }
+        })}
       </Menu>
-      <Button icon={'plus'} size={'mini'}></Button>
+      <Button icon={'plus'} size={'mini'} handler={addTabHandler}></Button>
     </TabContainer>
   );
 }
