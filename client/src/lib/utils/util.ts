1bc8fe0514e73bb23c50a04f7c155587bf2851bb
geonwoomun <38036598+geonwoomun@users.noreply.github.com>
2020-11-27 10:01:31 +0900

    [refactor] createAction 함수 리팩토링
    createAction 함수가 redux에서 기본적으로 제공하는 Action 타입을 사용하도록 리팩토링하였습니다.

--- client/src/lib/utils/util.ts
+++ client/src/lib/utils/util.ts
@@ -1,5 +1,7 @@
-export function createAction<T, P>(type: T): (payload: P) => { type: T; payload: P };
-export function createAction<T>(type: T): () => { type: T };
+import { Action } from 'redux';
+
+export function createAction<T, P>(type: T): (payload: P) => Action<T> & { payload: P };
+export function createAction<T>(type: T): () => Action<T>;
 export function createAction<T, P>(type: T) {
   return (payload?: P) => {
     return {
