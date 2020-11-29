235174f0247b3b03f98a706758fa1d15beb233be
geonwoomun <38036598+geonwoomun@users.noreply.github.com>
2020-11-29 21:02:46 +0900

    [fix] 드롭박스 안 없어지는 버그 수정

--- client/src/components/Meal/FormulaList/index.tsx
+++ client/src/components/Meal/FormulaList/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Header, Popup, Grid } from 'semantic-ui-react';
 // import * as ButtonImage from '../../../../public/image/math.svg';
 import DropDownItem from '../../Ingredients/DropDownItem';
@@ -13,31 +13,46 @@ import FormulaItem from '../../Ingredients/FormulaItem';
 
 function FormulaList() {
   const formulaRef = useRef<null | HTMLHeadingElement>(null);
+  const containerRef = useRef<null | HTMLDivElement>(null);
+  const timer = useRef<any>(null);
   const [nowFormulas, setNowFormula] = useState<LatexContent[]>([]);
-  const mouseHandler = (event: React.MouseEvent) => {
+
+  const displayFormula = () => {
     if (formulaRef.current) {
       formulaRef.current.style.display = 'flex';
+      timer.current = setTimeout(() => {
+        formulaRef.current!.style.display = 'none';
+      }, 500);
     }
   };
-  const leaveHandler = (event: React.MouseEvent) => {
+
+  const clearHiddenTimemout = () => {
+    clearTimeout(timer.current);
+  };
+
+  const hiddenFormula = () => {
     if (formulaRef.current) {
       formulaRef.current.style.display = 'none';
     }
   };
-  document.body.addEventListener('mouseleave', () => {
-    if (formulaRef.current) {
-      formulaRef.current.style.display = 'none';
-    }
-  });
+
+  useEffect(() => {
+    document.body.addEventListener('mouseleave', () => {
+      if (formulaRef.current) {
+        formulaRef.current.style.display = 'none';
+      }
+    });
+  }, []);
+
   return (
     <>
-      <S.FormulaContainer>
+      <S.FormulaContainer ref={containerRef} className="formula_container">
         <S.FormulaHeaderWrapper>
           {FORMULA_HEADER.map((latex, index) => (
             <DropDownItem
               latex={latex}
               key={index}
-              onMouseOver={mouseHandler}
+              onMouseOver={displayFormula}
               setNowFormula={setNowFormula}
             ></DropDownItem>
           ))}
@@ -47,15 +62,19 @@ function FormulaList() {
             <DropDownItem
               latex={latex}
               key={index}
-              onMouseOver={mouseHandler}
+              onMouseOver={displayFormula}
               setNowFormula={setNowFormula}
             ></DropDownItem>
           ))}
         </S.SymbolHeaderWrapper>
       </S.FormulaContainer>
-      <S.Contents ref={formulaRef} onMouseLeave={leaveHandler}>
+      <S.Contents ref={formulaRef} onMouseLeave={hiddenFormula} onMouseOver={clearHiddenTimemout}>
         {nowFormulas.map((latexInfo, index) => (
-          <FormulaItem key={index} latexInfo={latexInfo}></FormulaItem>
+          <FormulaItem
+            key={index}
+            latexInfo={latexInfo}
+            hiddenFormula={hiddenFormula}
+          ></FormulaItem>
         ))}
       </S.Contents>
     </>
