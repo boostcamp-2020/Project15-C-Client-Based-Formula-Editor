cdbe4b6189d81ae882a5440d4669d8ff585aa041
qkrrlgh519 <qkrrlgh519@naver.com>
2020-12-02 23:52:25 +0900

    [feat] AlertItem 컴포넌트 구현
    - 리스트 항목이 없거나, 로그인이 필요한 경우 리스트에 사용

---
+++ client/src/components/Ingredients/AlertItem/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import * as S from './style';
+import { Icon } from 'semantic-ui-react';
+import { AlertMessage } from '@constants/constants';
+
+interface AlertItemProps {
+  icon: any;
+  message: AlertMessage;
+}
+
+function AlertItem({ icon, message }: AlertItemProps) {
+  return (
+    <S.AlertItemContainer>
+      <S.IconBox>
+        <Icon name={icon} size="huge" />
+      </S.IconBox>
+      <S.MessageBox>{message}</S.MessageBox>
+    </S.AlertItemContainer>
+  );
+}
+
+export default AlertItem;
