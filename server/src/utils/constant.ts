2799951b126fef0c04ff5d719973aba0d5e84835
geonwoomun <38036598+geonwoomun@users.noreply.github.com>
2020-12-03 00:20:36 +0900

    [feat] 상태코드, 에러메시지 상수로 추가

---
+++ server/src/utils/constant.ts
@@ -0,0 +1,11 @@
+export const STATUS_CODE = {
+  success: 200,
+  created: 201,
+  clientError: 400,
+  serverError: 500,
+};
+
+export const ERROR_MESSAGE = {
+  clientError: 'Request Error',
+  serverError: 'Server Error',
+};
