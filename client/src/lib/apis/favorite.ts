1c9a1b14736b811ead4ebaaa25970bb94ac32ab9
qkrrlgh519 <qkrrlgh519@naver.com>
2020-12-02 23:50:40 +0900

    [feat] 즐겨찾기를 생성하는 axios 로직 구현

--- client/src/lib/apis/favorite.ts
+++ client/src/lib/apis/favorite.ts
@@ -1,5 +1,11 @@
 import { API } from './common';
 
+interface FavoriteBody {
+  title: string;
+  latex: string;
+  userId: number;
+}
+
 export const getFavorites = async (id: number) => {
   try {
     const response = await API.get(`/favorite/${id}`);
@@ -12,6 +18,15 @@ export const getFavorites = async (id: number) => {
 export const deleteFavorite = async (id: number) => {
   try {
     const response = await API.delete(`/favorite/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const createFavorite = async (body: FavoriteBody) => {
+  try {
+    const response = await API.post(`/favorite`, body);
     return response.data;
   } catch (error) {
     console.error(error);
