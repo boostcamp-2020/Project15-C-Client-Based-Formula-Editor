50c6ecd5481a531d873e6ac96e5c5a278f814557
qkrrlgh519 <qkrrlgh519@naver.com>
2020-12-06 23:52:55 +0900

    [feat] server쪽 auth 관련 router와 controller 구현

---
+++ server/src/controllers/auth-controller.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express';
+import { ValidateType, isValidateType } from '@utils/validator';
+import { STATUS_CODE, ERROR_MESSAGE } from '@utils/constant';
+import AuthService from '../service/auth-service';
+
+const AuthController = {
+  async login(req: Request, res: Response) {
+    try {
+      const { code } = req.body;
+      if (!isValidateType(code, ValidateType.String)) {
+        return res.status(STATUS_CODE.CLIENT_ERROR).json({ message: ERROR_MESSAGE.CLIENT_ERROR });
+      }
+
+      const results = await AuthService.getInstance().login(code);
+      res.status(STATUS_CODE.SUCCESS).json({ userData: results });
+    } catch (err) {
+      res.status(STATUS_CODE.SERVER_ERROR).json({ message: ERROR_MESSAGE.SERVER_ERROR });
+    }
+  },
+};
+
+export default AuthController;
--- server/src/router/index.ts
+++ server/src/router/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import favoriteRouter from './favorite-router';
+import authRouter from './auth-router';
 
 const router = express.Router();
 
+router.use('/auth', authRouter);
 router.use('/favorite', favoriteRouter);
 
 export default router;
