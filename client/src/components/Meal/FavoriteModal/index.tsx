177dc2e591da34f0d2c0d74797f4937e893958a7
dooking <123456ghghgh@naver.com>
2020-12-09 00:41:14 +0900

    [style] Favorite Modal Style 수정

--- client/src/components/Meal/FavoriteModal/index.tsx
+++ client/src/components/Meal/FavoriteModal/index.tsx
@@ -17,28 +17,30 @@ function FavoriteModal() {
 
   return (
     <>
-      <S.FavoriteModalContainer>
-        <S.FavoriteModalOverlay onClick={onClickModalClosed} />
-        <S.FavoriteModalWrapper>
-          <S.ModalTitle>즐겨찾기 등록</S.ModalTitle>
-          <S.ModalContent>
-            <S.ContentLabel>Title</S.ContentLabel>
-            <input type="text" onChange={onChangeTitle} value={title}></input>
-            <S.ContentLabel>Latex</S.ContentLabel>
-            <S.LatexBox>{currentTabInfo.latex}</S.LatexBox>
-          </S.ModalContent>
-          <S.ModalButtons>
-            <ButtonItem title={'취소'} handler={onClickModalClosed} color={'red'} />
-            <ButtonItem
-              title={'등록'}
-              color={'green'}
-              handler={() => {
-                onClickRegister({ userId, title, latex: currentTabInfo.latex }, clearTitle);
-              }}
-            />
-          </S.ModalButtons>
-        </S.FavoriteModalWrapper>
-      </S.FavoriteModalContainer>
+      {isOpen && (
+        <S.FavoriteModalContainer>
+          <S.FavoriteModalOverlay onClick={onClickModalClosed} />
+          <S.FavoriteModalWrapper>
+            <S.ModalTitle>즐겨찾기 등록</S.ModalTitle>
+            <S.ModalContent>
+              <S.ContentLabel>Title</S.ContentLabel>
+              <input type="text" onChange={onChangeTitle} value={title}></input>
+              <S.ContentLabel>Latex</S.ContentLabel>
+              <S.LatexBox>{currentTabInfo.latex}</S.LatexBox>
+            </S.ModalContent>
+            <S.ModalButtons>
+              <ButtonItem title={'취소'} handler={onClickModalClosed} color={'red'} />
+              <ButtonItem
+                title={'등록'}
+                color={'green'}
+                handler={() => {
+                  onClickRegister({ userId, title, latex: currentTabInfo.latex }, clearTitle);
+                }}
+              />
+            </S.ModalButtons>
+          </S.FavoriteModalWrapper>
+        </S.FavoriteModalContainer>
+      )}
     </>
   );
 }
