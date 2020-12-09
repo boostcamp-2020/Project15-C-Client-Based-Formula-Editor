e1b07921fffcdb139d9976897dd0fce60f8f747b
dooking <123456ghghgh@naver.com>
2020-12-10 01:35:24 +0900

    [feat] 저장, naver login 버튼 추가

--- client/src/components/Meal/MenuBar/index.tsx
+++ client/src/components/Meal/MenuBar/index.tsx
@@ -1,15 +1,52 @@
 import React from 'react';
 import TextAreaItem from '@ingredients/TextAreaItem';
+import OutputFormula from '@meal/OutputFormula';
+import { Button } from 'semantic-ui-react';
 
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '@contexts/index';
 import * as S from './style';
+import { setToken } from '@utils/token';
+import { userLogin, userLogout } from '@contexts/user';
 
 function MenuBar() {
+  const { userInfo } = useSelector((state: RootState) => state.user);
+  const { userId } = userInfo;
+  const dispatch = useDispatch();
+  const onClickLoginHandler = async () => {
+    chrome.runtime.sendMessage({ message: 'login' }, (response) => {
+      const { userToken, userId } = response.results;
+      setToken(userToken);
+      dispatch(userLogin(userId));
+    });
+  };
+
+  const onClickLogoutHandler = async () => {
+    chrome.storage.sync.clear();
+    dispatch(userLogout());
+  };
   return (
     <S.MenuBarContainer>
       <S.Logo>
         <S.LogoImg src="/image/logo.png" />
       </S.Logo>
       <TextAreaItem width={'30%'} />
+      <S.OutputWrapper>
+        <OutputFormula />
+      </S.OutputWrapper>
+      <S.ButtonWrapper>
+        <Button.Group basic size="massive">
+          <Button icon="file text outline" size="massive" />
+          <Button icon="file image outline" size="massive" />
+          <Button icon="qrcode" size="massive" />
+          <Button icon="tv" size="massive" />
+        </Button.Group>
+      </S.ButtonWrapper>
+      {userId ? (
+        <S.Img src="image/logout.png" onClick={onClickLogoutHandler} />
+      ) : (
+        <S.Img src="image/login.png" onClick={onClickLoginHandler} />
+      )}
     </S.MenuBarContainer>
   );
 }
--- client/src/components/Meal/MenuBar/style.ts
+++ client/src/components/Meal/MenuBar/style.ts
@@ -7,6 +7,29 @@ export const MenuBarContainer = styled.div`
   display: flex;
 `;
 
+export const OutputWrapper = styled.div`
+  width: 30%;
+`;
+export const ButtonWrapper = styled.div`
+  & div {
+    height: 100%;
+  }
+  & div > button {
+    height: 100%;
+    padding: 10px !important;
+    align-self: center;
+    & i {
+      font-size: 2em;
+    }
+  }
+`;
+export const IconWrapper = styled.div`
+  width: 50px;
+  & i {
+    width: 100% !important;
+  }
+`;
+
 export const Logo = styled.div`
   height: 100%;
   width: 65px;
@@ -16,4 +39,14 @@ export const Logo = styled.div`
 export const LogoImg = styled.img`
   margin-top: 5px;
   width: 65px;
+`;
+
+export const Img = styled.img`
+  border: 0.1px solid #d4d4d5;
+  border-radius: 5px;
+  margin-top: 6px;
+  width: 114px;
+  &:hover {
+    cursor: pointer;
+  }
 `;
