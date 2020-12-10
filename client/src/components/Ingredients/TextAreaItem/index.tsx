cee7dc2dd50572476d3ce7e10a727ce2f372eccc
geonwoomun <38036598+geonwoomun@users.noreply.github.com>
2020-12-10 10:42:48 +0900

    [feat] 검색어 추천 구현

--- client/src/components/Ingredients/TextAreaItem/index.tsx
+++ client/src/components/Ingredients/TextAreaItem/index.tsx
@@ -1,11 +1,39 @@
+import RecommendItem from '@ingredients/RecommendItem';
 import React from 'react';
-import { TextArea } from './style';
+import { TextArea, RecommendContainer } from './style';
 import useTextAreaItem from './useTextAreaItem';
 
 function TextAreaItem() {
-  const { currentTabInfo, onChangeHandler } = useTextAreaItem();
+  const {
+    currentTabInfo,
+    onChangeHandler,
+    isShow,
+    onKeyPress,
+    recommend,
+    clearAndCloseRecommend,
+  } = useTextAreaItem();
 
-  return <TextArea value={currentTabInfo.latex} onChange={onChangeHandler} />;
+  return (
+    <>
+      <TextArea value={currentTabInfo.latex} onChange={onChangeHandler} onKeyDown={onKeyPress} />
+      {isShow && (
+        <RecommendContainer>
+          추천
+          <ul>
+            {recommend.map((item, index) => (
+              <RecommendItem
+                key={`${item.latex}${index}`}
+                currentLatex={currentTabInfo.latex}
+                clearAndCloseRecommend={clearAndCloseRecommend}
+                latex={item.latex}
+                image={item.image}
+              />
+            ))}
+          </ul>
+        </RecommendContainer>
+      )}
+    </>
+  );
 }
 
 export default TextAreaItem;
--- client/src/components/Ingredients/TextAreaItem/useTextAreaItem.ts
+++ client/src/components/Ingredients/TextAreaItem/useTextAreaItem.ts
@@ -1,16 +1,45 @@
 import { useDispatch } from 'react-redux';
 import { editLatex } from '../../../contexts/latex';
 import useCurrentTab from '@hooks/useCurrentTab';
+import { useCallback, useState } from 'react';
+import { FORMULA_HEADER, LatexContent } from '@constants/latex-header';
 
 const useTextAreaItem = () => {
   const dispatch = useDispatch();
   const { currentTabInfo } = useCurrentTab();
+  const [isShow, setIsShow] = useState(false);
+  const [recommend, setRecommend] = useState<LatexContent[]>([]);
+
+  const clearAndCloseRecommend = useCallback(() => {
+    setIsShow(false);
+    setRecommend([]);
+  }, []);
+
+  const onKeyPress = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    const target = e.target as HTMLTextAreaElement;
+    if (e.key === 'Backspace' && target.value[target.value.length - 1] === '@') {
+      return clearAndCloseRecommend();
+    }
+
+    if (e.key === 'Enter') return clearAndCloseRecommend();
+    if (e.key === '@') {
+      setIsShow(true);
+    }
+  };
 
   const onChangeHandler = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    if (isShow) {
+      const startIndex = e.target.value.lastIndexOf('@') + 1;
+      const word = e.target.value.substring(startIndex);
+      const recommnedWords = FORMULA_HEADER.reduce<LatexContent[]>((acc, header) => {
+        return acc.concat(header.content.filter((item) => item.latex.includes(word)));
+      }, []);
+      setRecommend(recommnedWords);
+    }
     dispatch(editLatex({ latex: e.target.value }));
   };
 
-  return { currentTabInfo, onChangeHandler };
+  return { currentTabInfo, onChangeHandler, isShow, onKeyPress, recommend, clearAndCloseRecommend };
 };
 
 export default useTextAreaItem;
