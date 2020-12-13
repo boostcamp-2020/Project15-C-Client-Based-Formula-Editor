7f77b89a1441d28a9e7bbaaba3d3deba7dd12e23
WooDong <tmlu48@naver.com>
2020-12-13 17:06:35 +0900

    hader QR코드 제거 및 사이즈 수정

--- client/src/components/Meal/MenuBar/index.tsx
+++ client/src/components/Meal/MenuBar/index.tsx
@@ -16,13 +16,11 @@ function MenuBar({ onToggle }: ResizeHeaderProps) {
     imageUrl,
     downloadImage,
     downloadText,
-    createHandler,
     clipboardHandler,
   } = useSaveButtons();
   const MenuBarSaveButtons = [
     { title: '이미지 저장', iconName: 'image', size: 'massive', onClickHandler: downloadImage },
     { title: '텍스트 저장', iconName: 'file text', size: 'massive', onClickHandler: downloadText },
-    { title: 'QR코드 생성', iconName: 'qrcode', size: 'massive', onClickHandler: createHandler },
     { title: '화면 적용', iconName: 'tv', size: 'massive', onClickHandler: clipboardHandler },
   ];
   return (
--- client/src/components/Meal/MenuBarInput/style.ts
+++ client/src/components/Meal/MenuBarInput/style.ts
@@ -1,7 +1,7 @@
 import styled from '@emotion/styled';
 
 export const TextAreaItemWrapper = styled.div`
-  width: 30%;
+  width: 35%;
   margin: 10px;
   display: flex;
 `;
