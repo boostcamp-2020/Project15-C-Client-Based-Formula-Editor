59e4a3e571de87b67e6923cdb846b35801c0cf2d
qkrrlgh519 <qkrrlgh519@naver.com>
2020-12-14 18:01:50 +0900

    [fix] 수식이 아닌 값이 적용되도록 버그 수정

--- client/src/components/Meal/CalculatorTab/useCalculatorTab.ts
+++ client/src/components/Meal/CalculatorTab/useCalculatorTab.ts
@@ -21,7 +21,7 @@ const useCalculatorTab = () => {
 
   const onClickOutputHandler = (e: React.MouseEvent) => {
     if (mathfield) {
-      mathfield.write(outputOperation);
+      mathfield.write(outputResult);
     }
   };
 
