09d15b36666ced5bedb605dc950af17cc0658310
WooDong <tmlu48@naver.com>
2020-12-16 13:57:52 +0900

    [test] table 및 textarea test 완료

--- client/src/__tests__/QrCode.test.tsx
+++ client/src/__tests__/QrCode.test.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
+import { render } from '../lib/utils/test-util';
 import QrCode from '../components/Ingredients/QrCode/index';
 import '@testing-library/jest-dom';
 
@@ -7,5 +7,13 @@ describe('<QrCode />', () => {
   it('renders QrCode', () => {
     const { container } = render(<QrCode imageUrl={'imgUrl'} />);
     expect(container).toMatchSnapshot();
+  });
+  it('in canvers', () => {
+    const { container } = render(<QrCode imageUrl={'imgUrl'} />);
+    expect(container.querySelector('canvas')).toBeInTheDocument;
+    expect(container.querySelector('canvas')).toHaveStyle({
+      width: '128px',
+      height: '128px',
+    });
   });
 });
--- client/src/__tests__/TextAreaItem.test.tsx
+++ client/src/__tests__/TextAreaItem.test.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
+import { render } from '../lib/utils/test-util';
 import TextAreaItem from '../components/Ingredients/TextAreaItem/index';
 import '@testing-library/jest-dom';
 
@@ -7,5 +7,12 @@ describe('<TextAreaItem />', () => {
   it('renders TextAreaItem', () => {
     const { container } = render(<TextAreaItem size={'big'} />);
     expect(container).toMatchSnapshot();
+  });
+  it('in TextAreaItem', () => {
+    const { container } = render(<TextAreaItem size={'big'} />);
+    expect(container.querySelector('textarea')).toBeInTheDocument;
+    expect(container.querySelector('textarea')).toHaveStyle({
+      height: '100%',
+    });
   });
 });
